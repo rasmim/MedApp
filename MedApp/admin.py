8398521f2edc03f6b7d04df42b3be5cc62c3eb70
rasmim <mrasmivlad@gmail.com>
2017-06-29 22:54:55 +0300

    Added Chart; hash admin resolved

--- MedApp/admin.py
+++ MedApp/admin.py
@@ -1,10 +1,11 @@
+from django import forms
 from django.contrib import admin
-from django.contrib.auth.models import Group
-
-# Register your models here.
-from django.utils.html import format_html
+from django.contrib.auth.admin import UserAdmin
 
 from MedApp.models import CustomUser, Appointment, Patient, Specialization, Diagnostic, Consultation, ConsultationResult
+
+
+# Register your models here.
 
 
 class DoctorFilter(admin.SimpleListFilter):
@@ -54,15 +55,35 @@ class ConsultationEdit(admin.ModelAdmin):
 
 class ConsultationResultEdit(admin.ModelAdmin):
     list_display = ('patient','diagnostic','recipe','image_tag')
-
+    actions = ['print_consultation']
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'diagnostic':
             specialization_id = Specialization.objects.get(doctor_id=request.user.id)
             kwargs["queryset"] = Diagnostic.objects.filter(specilization_id=specialization_id)
         return super(ConsultationResultEdit, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-
-
+        # def print_consultation(request, canvas=None):
+        #     response = HttpResponse(content_type='application/pdf')
+        #     response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+        #
+        #     buffer = BytesIO()
+        #
+        #     # Create the PDF object, using the BytesIO object as its "file."
+        #     p = canvas.Canvas(buffer)
+        #
+        #     # Draw things on the PDF. Here's where the PDF generation happens.
+        #     # See the ReportLab documentation for the full list of functionality.
+        #     p.drawString(100, 100, "Hello world.")
+        #
+        #     # Close the PDF object cleanly.
+        #     p.showPage()
+        #     p.save()
+        #
+        #     # Get the value of the BytesIO buffer and write it to the response.
+        #     pdf = buffer.getvalue()
+        #     buffer.close()
+        #     response.write(pdf)
+        #     return response
 
 class DiagnosticEdit(admin.ModelAdmin):
     list_display = ('name', 'code')
@@ -95,15 +116,44 @@ admin.site.register(Consultation, ConsultationEdit)
 admin.site.register(Patient, PatientEdit)
 
 
-class FilterUser(admin.ModelAdmin):
-    def is_member(self, CustomUser):
-        if CustomUser.groups.filter(name='Doctors').exists():
-            pass
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
 
-    admin.site.register(Specialization)
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
-admin.site.register(CustomUser, FilterUser)
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    add_form = UserCreationForm
+    list_display = ("email",)
+    ordering = ("email",)
+    # refine the fields
+
+    fieldsets = (
+        (
+        None, {'fields': ('email', 'password', 'first_name', 'last_name', 'groups', 'last_login', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+            'email', 'password', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active', 'groups',
+            'last_login', 'user_permissions')}
+         ),
+    )
+
+    search_fields = ('first_name', 'last_name', 'email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 
 
--- MedApp/views.py
+++ MedApp/views.py
@@ -1,3 +1,73 @@
+from django.db.models import Count
 from django.shortcuts import render
 
-# Create your views here.
+from fusioncharts import FusionCharts
+from .models import *
+
+
+# The `chart` function is defined to generate Column 2D chart from database.
+def chart(request):
+    # Chart data is passed to the `dataSource` parameter, as dict, in the form of key-value pairs.
+    dataSource = {}
+    dataSource['chart'] = {
+        "caption": "Patients by specialization",
+        "subCaption": "MedApp Clinic",
+        "xAxisName": "Month",
+        "yAxisName": "Number of patients",
+        "theme": "zune"
+    }
+
+    # The data for the chart should be in an array where each element of the array is a JSON object
+    # having the `label` and `value` as key value pair.
+
+    dataSource['data'] = []
+    # Iterate through the data in `Revenue` model and insert in to the `dataSource['data']` list.
+
+    number_patient = Consultation.objects.all().values('specialization').annotate(
+        total=Count('specialization')).order_by('total')
+
+    for members in number_patient:
+        data = {}
+        specilization = Specialization.objects.get(pk=members['specialization'])
+        data['label'] = specilization.name
+
+        data['value'] = members['total']
+        dataSource['data'].append(data)
+        # number_patient = Consultation.objects.count(name='specialization')
+
+        # for key in Consultation.objects.all():
+        #     data = {}
+        #     data['label'] = key.specialization
+        #     data['value'] = key.
+
+
+        # Create an object for the Column 2D chart using the FusionCharts class constructor
+    column2D = FusionCharts("column2D", "ex1", "800", "600", "chart-1", "json", dataSource)
+    # column2d = FusionCharts()
+
+    return render(request, 'charts.html', {'output': column2D.render()})
+
+# def print_view(request):
+#     # Create the HttpResponse object with the appropriate PDF headers.
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+#
+#     buffer = BytesIO()
+#
+#     # Create the PDF object, using the BytesIO object as its "file."
+#     p = canvas.Canvas(buffer)
+#
+#     # Draw things on the PDF. Here's where the PDF generation happens.
+#     # See the ReportLab documentation for the full list of functionality.
+#     p.drawImage("medapp.bmp", 5, 750,mask='auto')
+#     p.drawCentredString(200,600,"Consultation results")
+#
+#     # Close the PDF object cleanly.
+#     p.showPage()
+#     p.save()
+#
+#     # Get the value of the BytesIO buffer and write it to the response.
+#     pdf = buffer.getvalue()
+#     buffer.close()
+#     response.write(pdf)
+#     return response
